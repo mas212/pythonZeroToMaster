45b6c5d7915c11b69e2291a45a9a10696ac4ec21
mas212 <mas32957@gmail.com>
2019-09-12 13:42:16 +0800

    string

---
+++ stringOperator.py
@@ -0,0 +1,15 @@
+x = "hallo"
+a = "jos"
+
+print(a.split(","))
+
+print('-'.join(x + a))
+b = 10
+c = 5
+
+ab = b * c
+print(ab)
+desc = "hallo pejuang development"
+print(desc.format("hallo", "pejuang development"))
+brief = "OKE METUP"
+print(brief.lower())
